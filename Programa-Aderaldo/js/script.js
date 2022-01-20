c83999f6d1abe639a100a08ab6826131d1ca89d6
Erison Cavalcante <erison7596e@gmail.com>
2022-01-20 16:22:26 -0300

    falta consertar o bug de envio, fica repetindo

---
+++ Programa-Aderaldo/js/script.js
@@ -0,0 +1,48 @@
+var quant;
+function gerarQuant(){
+    quant = document.getElementById("quantProd").value;
+    document.getElementById("quantidadeProdutos").style.display = 'none';
+    const escreve = document.getElementById("formInputNum");
+
+    for (let i = 0; i < quant; i++) {
+          
+        escreve.insertAdjacentHTML('beforeend', `<div class="teste"><label for="produto${i} class="nomeProduto">Produto ${i+1}  </label>
+        <input type="number" inputmode="decimal" step="0.1" required id="num${i}"></div>       <div class="gerados_2" id="divgerados_${i}">
+        
+            </div>
+            <div class="resultFinal" id="resultfinal${i}">
+                
+            </div><br> `);
+        
+        if(i == quant-1) {
+            escreve.insertAdjacentHTML('beforeend', '<br><input type="button" value="Enviar" class="botaoEnvi" onclick="gerarResult()">');
+        }
+    }
+}
+var quanti;
+const total = 0;
+function gerarPorcert(){
+    quanti = document.getElementById("porcent2").value;
+    quanti = quanti/100;
+    document.getElementById("porcent").style.display = 'none';
+}
+
+function gerarResult(){
+    var escreve;
+    var valor;
+    var escreve2;
+    let tot = 0;
+    for(let i = 0; i<quant; i++){
+        
+        escreve2 = document.getElementById(`resultfinal${i}`);
+        escreve= document.getElementById(`divgerados_${i}`);
+        valor= document.getElementById(`num${i}`).value;
+        console.log(valor);
+        let fim = parseFloat(valor)* parseFloat(quanti);
+        tot += parseFloat(valor) + parseFloat(fim);
+        console.log(tot);
+        
+        escreve.insertAdjacentHTML('beforeend', `<div class="escrevendo">${fim.toFixed(2)}</div>`);
+        escreve2.insertAdjacentHTML('beforeend', `<div class="escrevendo">${tot.toFixed(2)}</div>`);
+    }
+}
